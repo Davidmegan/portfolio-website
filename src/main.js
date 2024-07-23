b5b123d155b5ba3f5a0d958fdc973b6e86db9ad4
David Megan C <cdavidmegan@gmail.com>
2024-07-23 20:35:09 +0530

    added about

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import '../styles/modern-normalize.css'
+import '../styles/style.css'
+import '../styles/components/header.css'
+import '../styles/components/about.css'
+import '../styles/utils.css'
