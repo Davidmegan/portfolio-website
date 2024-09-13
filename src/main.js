270933b735e965ca5ddf06d2f370f0992ab18333
David Megan C <118439333+Davidmegan@users.noreply.github.com>
2024-09-13 22:26:57 +0530

    set up gsap and added glitch

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import { gsap } from 'gsap';
+import './utils/animations'
 import '../styles/modern-normalize.css'
 import '../styles/style.css'
 import '../styles/components/header.css'
@@ -6,10 +8,13 @@ import '../styles/components/projects.css'
 import '../styles/components/contact.css'
 import '../styles/components/profile.css'
 import '../styles/components/mobile-nav.css'
+import '../styles/components/test.css'
 import '../styles/utils.css'
+import '../styles/components/glitch.css'
 
 import './utils/mobile-nav'
 import './utils/projects'
 import mobileNav from './utils/mobile-nav'
 
 mobileNav();
+console.log(gsap);
---
+++ src/utils/animations.js
@@ -0,0 +1,6 @@
+import { gsap } from 'gsap';
+
+// gsap.to(".test", { duration: 3, x: 300 });
+gsap.from(".header__menu", {duration: 1, y: -50}) 
+gsap.from(".about__img", {duration: 1, x: 50})  
+gsap.from(".left", {duration: 1, x: -200}) 
